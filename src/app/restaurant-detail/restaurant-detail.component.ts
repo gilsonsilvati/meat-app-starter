5703ac935f7fc3ac1ca4314d9e488b102cd11a37
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-24 22:11:33 -0300

    Serviço HTTP para o Detalhe do Restaurante

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant;
+
+  constructor(private restaurantsService: RestaurantsService, private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.restaurantsService.restaurantById(this.router.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant);
   }
 
 }
