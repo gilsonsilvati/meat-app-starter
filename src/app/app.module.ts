da41b1a4d000040aed0d9fe3ccb700c795a05044
Gilson Silva <gilsonsilvati@gmail.com>
2020-08-31 22:21:38 -0300

    Localizando Preços para a Moeda Brasileira

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -39,7 +39,8 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
   ],
   providers: [
     RestaurantsService,
-    ShoppingCartService
+    ShoppingCartService,
+    {provide: LOCALE_ID, useValue: 'pt-BR'}
   ],
   bootstrap: [AppComponent]
 })
