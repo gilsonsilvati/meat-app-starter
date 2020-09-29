b36429a7635e7af25e4d40318a9b18c68609c5b6
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-29 17:48:37 -0300

    Utilizando HTTP Interceptors

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -9,12 +9,11 @@ import { MEAT_API } from '../app.api';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { Order } from './order.model';
-import { LoginService } from './../security/login/login.service';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private cartService: ShoppingCartService, private http: HttpClient, private loginService: LoginService) { }
+  constructor(private cartService: ShoppingCartService, private http: HttpClient) { }
 
   itemsValue(): number {
     return this.cartService.total();
@@ -41,13 +40,7 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    let headers = new HttpHeaders();
-
-    if (this.loginService.isLoggedIn) {
-        headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`);
-    }
-
-    return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers: headers })
+    return this.http.post<Order>(`${MEAT_API}/orders`, order)
       .map(o => o.id);
   }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { InputComponent } from './input/input.component';
 import { RadioComponent } from './radio/radio.component';
@@ -14,44 +15,46 @@ import { NotificationService } from './messages/notification.service';
 import { LoginService } from '../security/login/login.service';
 import { LoggedInGuard } from './../security/loggedin.guard';
 import { LeaveOrderGuard } from './../order/leave-order.guard';
+import { AuthInterceptor } from './../security/auth.interceptor';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  declarations: [
-    InputComponent,
-    RadioComponent,
-    RatingComponent,
-    SnackbarComponent
-  ],
-  exports: [
-    InputComponent,
-    RadioComponent,
-    RatingComponent,
-    SnackbarComponent,
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ]
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    declarations: [
+        InputComponent,
+        RadioComponent,
+        RatingComponent,
+        SnackbarComponent
+    ],
+    exports: [
+        InputComponent,
+        RadioComponent,
+        RatingComponent,
+        SnackbarComponent,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class SharedModule {
 
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: SharedModule,
-      providers: [
-        RestaurantsService,
-        ShoppingCartService,
-        OrderService,
-        NotificationService,
-        LoginService,
-        LoggedInGuard,
-        LeaveOrderGuard
-      ]
-    };
-  }
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [
+                RestaurantsService,
+                ShoppingCartService,
+                OrderService,
+                NotificationService,
+                LoginService,
+                LoggedInGuard,
+                LeaveOrderGuard,
+                { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+            ]
+        };
+    }
 
 }
