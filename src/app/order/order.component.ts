46d8a57e9795288d0fdd8dc8ce708d7b76b147de
Gilson Silva <gilsonsilvati@gmail.com>
2020-09-30 11:19:37 -0300

    Mudando a Atualização de Formulários (updateOn)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
@@ -36,15 +36,17 @@ export class OrderComponent implements OnInit {
     constructor(private orderService: OrderService, private router: Router, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
-        this.orderForm = this.formBuilder.group({
-            name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+        this.orderForm = new FormGroup({
+            name: new FormControl('', { validators:
+                [Validators.required, Validators.minLength(5)]
+            }),
             email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
             emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
             address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
             number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
             optionalAddress: this.formBuilder.control(''),
             paymentOption: this.formBuilder.control('', [Validators.required])
-        }, { validator: OrderComponent.equalsTo });
+        }, { validators: [OrderComponent.equalsTo], updateOn: 'blur' });
     }
 
     // tslint:disable-next-line: member-ordering
